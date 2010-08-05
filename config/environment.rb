6e4ef04acf6867735dbb17ac07935de3ffe63ea8
smsohan <sohan39@gmail.com>
2010-08-05 15:37:12 -0600

    added mocha gem in config

--- config/environment.rb
+++ config/environment.rb
@@ -21,6 +21,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "authlogic"
   config.gem "crummy"
+  config.gem "mocha"
   # config.gem(
   #   'thinking-sphinx',
   #   :lib     => 'thinking_sphinx',
