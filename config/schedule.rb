b751b528734d975078b0aa6c3958ee9f70531ce8
smsohan <sohan39@gmail.com>
2010-09-16 16:34:13 -0600

    whenever now defaults to production

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
-set :path, '/Users/smsohan/Taggy' 
-set :environment, :development
+# set :path, '/Users/smsohan/Taggy' 
+# set :environment, :development
 
 every 1.minute do
   runner "Project.grab_all_emails!"
