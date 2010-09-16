e26ac1cb6c64ec3f46362e009d0993a821b18cce
smsohan <sohan39@gmail.com>
2010-09-16 16:06:20 -0600

    removed gems install from deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -51,13 +51,13 @@ namespace :deploy do
   before "deploy", "deploy:cleanup"
   %w(start restart).each { |name| task name do passenger.restart end }
   after "deploy:update_code", "deploy:symlink_shared_files_and_dirs"
-  after "deploy:symlink_shared_files_and_dirs", "gems:install"
+  # after "deploy:symlink_shared_files_and_dirs", "gems:install"
   after "gems:install", "solr:restart"
   after "solr:restart", "crontab:update"
   
   desc "Symlink shared files and directories."
   task :symlink_shared_files_and_dirs do
-    run "ln -s #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -s #{shared_path}/config/database_.yml #{release_path}/config/database.yml"
   end
 
 end
